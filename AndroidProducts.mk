5cfb5821f13a5b9f4bb22125a467b3eb5859a158
haikaru17 <haikalluthfianinobalukia@proton.me>
2025-10-20 13:02:08 +0000

    whyred: xdroidCLO bringup.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_whyred.mk
+    $(LOCAL_DIR)/xdroid_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_whyred-user \
-    aosp_whyred-userdebug \
-    aosp_whyred-eng
+    xdroid_whyred-user \
+    xdroid_whyred-userdebug \
+    xdroid_whyred-eng
--- xdroid_whyred.mk
+++ xdroid_whyred.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xdroid/config/common.mk)
 
 # PixelExperience Properties
 TARGET_INCLUDE_WIFI_EXT := true
@@ -27,8 +27,10 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := xdroid_whyred
 PRODUCT_MODEL := Redmi Note 5
+XDROID_BOOT_DARK := true
+XDROID_UI_BLUR := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
