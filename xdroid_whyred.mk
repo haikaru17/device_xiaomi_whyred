97b749732663473de2b80b3fa6389d619b22dfe5
haikaru17 <haikalluthfianinobalukia@proton.me>
2025-10-22 10:37:28 +0000

    whyred: Add keys for signed build

--- xdroid_whyred.mk
+++ xdroid_whyred.mk
@@ -24,6 +24,9 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Inherit from custom vendor.
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
+# Keys
+-include vendor/extra/product.mk
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
